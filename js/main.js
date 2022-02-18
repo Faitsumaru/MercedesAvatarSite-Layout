9743ed51a43cd6a39a786f1670a4d3592c7a920c
Gleb <kgm20011505@mail.ru>
2022-02-18 23:26:29 +0300

    1 block <header> is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
 
+    $('.header__nav').on('click', function(){
+        $('.header__nav').toggleClass('header__nav--active');
+        $('.menu__dropdown-list').toggleClass('menu__dropdown-list--active');
+
+        //change img on burger
+        var img = 'images/menu-btn.svg';
+        var activeImg = 'images/menu-btn--active.svg';
+        $('.header__nav-img').attr("src", $('.header__nav-img').attr("src") === img ? activeImg : img);
+        $(this).css({
+            transition: '.4s linear'
+        });
+    });
+
 });
