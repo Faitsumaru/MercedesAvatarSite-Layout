4f16c354a159592cee90d043aa33774ecd2beb03
Gleb <kgm20011505@mail.ru>
2022-02-19 17:33:24 +0300

    4 block <video> is done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.header__nav').on('click', function(){
+    $('.header__nav').on('click', function() {
         $('.header__nav').toggleClass('header__nav--active');
         $('.menu__dropdown-list').toggleClass('menu__dropdown-list--active');
 
@@ -8,9 +8,22 @@ $(function(){
         var img = 'images/menu-btn.svg';
         var activeImg = 'images/menu-btn--active.svg';
         $('.header__nav-img').attr("src", $('.header__nav-img').attr("src") === img ? activeImg : img);
-        $(this).css({
-            transition: '.4s linear'
-        });
     });
+
+    //video playing
+    var vid = $('.popup-vid'); 
+    vid.magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+    // var vid = $('video');
+    // $('.video__play-btn').on('click', function() {
+    //     vid.toggleClass('vid--active');
+    //     vid.attr("controls");
+    // });
 
 });
